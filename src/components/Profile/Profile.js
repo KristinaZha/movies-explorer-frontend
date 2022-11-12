743e3f845986116131d9b3d0471334fa07517fcc
Kristina <kristinka288@gmail.com>
2022-11-12 17:32:29 +0300

    2

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from "react";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from "react";
 import {useLocation} from 'react-router-dom';
 
